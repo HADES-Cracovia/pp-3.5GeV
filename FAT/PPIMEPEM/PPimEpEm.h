b076d5ad7e0bb8fcf41b035a245bbe4f957d8a2a
krzysn <k.nowakowski@doctoral.uj.edu.pl>
2019-11-27 15:20:10 +0100

    add pim variables

--- FAT/PPIMEPEM/PPimEpEm.h
+++ FAT/PPIMEPEM/PPimEpEm.h
@@ -279,7 +279,76 @@ public :
    Float_t         p_track_length;
    Float_t         p_tracklength;
    Float_t         p_z;
-   Float_t         runnumber;
+
+   Float_t         pim_beta;
+   Float_t         pim_beta_new;
+   Float_t         pim_btChargeRing;
+   Float_t         pim_btChargeSum;
+   Float_t         pim_btChi2;
+   Float_t         pim_btClusters;
+   Float_t         pim_btMaxima;
+   Float_t         pim_btMaximaCharge;
+   Float_t         pim_btMaximaChargeShared;
+   Float_t         pim_btMaximaChargeSharedFragment;
+   Float_t         pim_btMaximaShared;
+   Float_t         pim_btMaximaSharedFragment;
+   Float_t         pim_btMeanDist;
+   Float_t         pim_btNearbyMaxima;
+   Float_t         pim_btNearbyMaximaShared;
+   Float_t         pim_btPadsClus;
+   Float_t         pim_btPadsRing;
+   Float_t         pim_btRingMatrix;
+   Float_t         pim_dedx_mdc;
+   Float_t         pim_dedx_tof;
+   Float_t         pim_id;
+   Float_t         pim_isBT;
+   Float_t         pim_isOffVertexClust;
+   Float_t         pim_isPrimaryVertex;
+   Float_t         pim_isUsedVertex;
+   Float_t         pim_isring;
+   Float_t         pim_isringmdc;
+   Float_t         pim_isringnomatch;
+   Float_t         pim_isringtrack;
+   Float_t         pim_kIsLepton;
+   Float_t         pim_kIsUsed;
+   Float_t         pim_mdcinnerchi2;
+   Float_t         pim_mdcouterchi2;
+   Float_t         pim_oa_hadr;
+   Float_t         pim_oa_lept;
+   Float_t         pim_p;
+   Float_t         pim_p_corr_em;
+   Float_t         pim_p_corr_ep;
+   Float_t         pim_p_corr_p;
+   Float_t         pim_p_corr_pim;
+   Float_t         pim_p_corr_pip;
+   Float_t         pim_phi;
+   Float_t         pim_phi_rich;
+   Float_t         pim_pid;
+   Float_t         pim_q;
+   Float_t         pim_r;
+   Float_t         pim_resolution;
+   Float_t         pim_resoultion;
+   Float_t         pim_rich_amp;
+   Float_t         pim_rich_centr;
+   Float_t         pim_rich_houtra;
+   Float_t         pim_rich_padnum;
+   Float_t         pim_rich_patmat;
+   Float_t         pim_rkchi2;
+   Float_t         pim_sector;
+   Float_t         pim_shw_sum0;
+   Float_t         pim_shw_sum1;
+   Float_t         pim_shw_sum2;
+   Float_t         pim_system;
+   Float_t         pim_theta;
+   Float_t         pim_theta_rich;
+   Float_t         pim_tof_mom;
+   Float_t         pim_tof_new;
+   Float_t         pim_tof_rec;
+   Float_t         pim_track_length;
+   Float_t         pim_tracklength;
+   Float_t         pim_z;
+
+  Float_t         runnumber;
    Float_t         totalmult;
    Float_t         trigbit;
    Float_t         trigdec;
@@ -547,7 +616,75 @@ public :
    TBranch        *b_p_track_length;   //!
    TBranch        *b_p_tracklength;   //!
    TBranch        *b_p_z;   //!
-   TBranch        *b_runnumber;   //!
+TBranch        *b_pim_beta;   //!
+   TBranch        *b_pim_beta_new;   //!
+   TBranch        *b_pim_btChargeRing;   //!
+   TBranch        *b_pim_btChargeSum;   //!
+   TBranch        *b_pim_btChi2;   //!
+   TBranch        *b_pim_btClusters;   //!
+   TBranch        *b_pim_btMaxima;   //!
+   TBranch        *b_pim_btMaximaCharge;   //!
+   TBranch        *b_pim_btMaximaChargeShared;   //!
+   TBranch        *b_pim_btMaximaChargeSharedFragment;   //!
+   TBranch        *b_pim_btMaximaShared;   //!
+   TBranch        *b_pim_btMaximaSharedFragment;   //!
+   TBranch        *b_pim_btMeanDist;   //!
+   TBranch        *b_pim_btNearbyMaxima;   //!
+   TBranch        *b_pim_btNearbyMaximaShared;   //!
+   TBranch        *b_pim_btPadsClus;   //!
+   TBranch        *b_pim_btPadsRing;   //!
+   TBranch        *b_pim_btRingMatrix;   //!
+   TBranch        *b_pim_dedx_mdc;   //!
+   TBranch        *b_pim_dedx_tof;   //!
+   TBranch        *b_pim_id;   //!
+   TBranch        *b_pim_isBT;   //!
+   TBranch        *b_pim_isOffVertexClust;   //!
+   TBranch        *b_pim_isPrimaryVertex;   //!
+   TBranch        *b_pim_isUsedVertex;   //!
+   TBranch        *b_pim_isring;   //!
+   TBranch        *b_pim_isringmdc;   //!
+   TBranch        *b_pim_isringnomatch;   //!
+   TBranch        *b_pim_isringtrack;   //!
+   TBranch        *b_pim_kIsLepton;   //!
+   TBranch        *b_pim_kIsUsed;   //!
+   TBranch        *b_pim_mdcinnerchi2;   //!
+   TBranch        *b_pim_mdcouterchi2;   //!
+   TBranch        *b_pim_oa_hadr;   //!
+   TBranch        *b_pim_oa_lept;   //!
+   TBranch        *b_pim_p;   //!
+   TBranch        *b_pim_p_corr_em;   //!
+   TBranch        *b_pim_p_corr_ep;   //!
+   TBranch        *b_pim_p_corr_p;   //!
+   TBranch        *b_pim_p_corr_pim;   //!
+   TBranch        *b_pim_p_corr_pip;   //!
+   TBranch        *b_pim_phi;   //!
+   TBranch        *b_pim_phi_rich;   //!
+   TBranch        *b_pim_pid;   //!
+   TBranch        *b_pim_q;   //!
+   TBranch        *b_pim_r;   //!
+   TBranch        *b_pim_resolution;   //!
+   TBranch        *b_pim_resoultion;   //!
+   TBranch        *b_pim_rich_amp;   //!
+   TBranch        *b_pim_rich_centr;   //!
+   TBranch        *b_pim_rich_houtra;   //!
+   TBranch        *b_pim_rich_padnum;   //!
+   TBranch        *b_pim_rich_patmat;   //!
+   TBranch        *b_pim_rkchi2;   //!
+   TBranch        *b_pim_sector;   //!
+   TBranch        *b_pim_shw_sum0;   //!
+   TBranch        *b_pim_shw_sum1;   //!
+   TBranch        *b_pim_shw_sum2;   //!
+   TBranch        *b_pim_system;   //!
+   TBranch        *b_pim_theta;   //!
+   TBranch        *b_pim_theta_rich;   //!
+   TBranch        *b_pim_tof_mom;   //!
+   TBranch        *b_pim_tof_new;   //!
+   TBranch        *b_pim_tof_rec;   //!
+   TBranch        *b_pim_track_length;   //!
+   TBranch        *b_pim_tracklength;   //!
+   TBranch        *b_pim_z;   //!
+
+  TBranch        *b_runnumber;   //!
    TBranch        *b_totalmult;   //!
    TBranch        *b_trigbit;   //!
    TBranch        *b_trigdec;   //!
