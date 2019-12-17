29dfbd20f513aa149c6c44df6cd8dbbc786e0cc9
krzysn <k.nowakowski@doctoral.uj.edu.pl>
2019-12-17 11:23:39 +0100

    add files

---
+++ FAT/PPIMPIPPIM/All_analysis_backup/createHistos.C
@@ -0,0 +1,194 @@
+#define createHistos_cxx
+#include "createHistos.h"
+#include <TCutG.h>
+#include <TH2.h>
+#include <TStyle.h>
+#include <TCanvas.h>
+#include <TMath.h>
+#include <TGraphErrors.h>
+#include <TH1F.h>
+#include <TFile.h>
+#include <iostream>
+#include <TF1.h>
+
+using namespace std;
+
+void createHistos::Loop(char* output)
+{
+  //   In a ROOT session, you can do:
+  //      root> .L createHistos.C
+  //      root> createHistos t
+  //      root> t.GetEntry(12); // Fill t data members with entry number 12
+  //      root> t.Show();       // Show values of entry 12
+  //      root> t.Show(16);     // Read and show values of entry 16
+  //      root> t.Loop();       // Loop on all entries
+  //
+
+  //     This is the loop skeleton where:
+  //    jentry is the global entry number in the chain
+  //    ientry is the entry number in the current Tree
+  //  Note that the argument to GetEntry must be:
+  //    jentry for TChain::GetEntry
+  //    ientry for TTree::GetEntry and TBranch::GetEntry
+  //
+  //       To read only selected branches, Insert statements like:
+  // METHOD1:
+  //    fChain->SetBranchStatus("*",0);  // disable all branches
+  //    fChain->SetBranchStatus("branchname",1);  // activate branchname
+  // METHOD2: replace line
+  //    fChain->GetEntry(jentry);       //read all branches
+  //by  b_branchname->GetEntry(ientry); //read only this branch
+  if (fChain == 0) return;
+  const int bin=200;
+  const int xmin=1000;
+  const int xmax=2000;
+  const int nsignal=20;
+  double sidebandmin=10;
+  double sidebandmax=20;
+  int step;
+  TH1F* signal=new TH1F("signal","signal simulated from gaus",bin,xmin,xmax);
+  TH1F* background=new TH1F("background","background from side-band;M^{inv}_{p #pi- #pi+ #pi-}[MeV]",bin,xmin,xmax);
+  TH1F* data=new TH1F("data","data from experiment;M^{inv}_{p #pi- #pi+ #pi-}[MeV]",bin,xmin,xmax);
+  TH1F* oryginal_spectrum=new TH1F("oryginal_spectrum","oryginal spectrum for side-band;M^{inv}_{p #pi-}[MeV]",bin*6,xmin,xmax);
+  TGraphErrors* resi=new TGraphErrors(bin);
+  TF1* background_fit=new TF1("background_fit","pol2(0)",1000,1200);
+  TH1F* missing_mass_K0_L=new TH1F("missing_mass_K0_L","missing mass for #Lambda K^{0} candidates",1000,600,1600);
+  TH2F* dedx_lambda=new TH2F("dedx_lambda","de/dx for #Lambda events",250,0,2000,250,0,18);
+  TH2F* miss_m_vs_pip_pim=new TH2F("miss_m_vs_pip_pim","M^{miss} vs. M_{#pi+ #pi-}",50,1340,1650,50,200,450);
+
+  TFile *cutFile=new TFile("/lustre/hades/user/knowakow/PP/FAT/PPIMPIPPIM_sim/TMVAeval_DD/cut_miss_mass_vs_pip_pim.root","READ");
+  //TFile *cutFile=new TFile("/lustre/hades/user/knowakow/PP/FAT/PPIMPIPPIM_sim/TMVAeval_DD/cut_miss_pip_pim_tight.root","READ");
+  TCutG *graph_cut=0;
+  cutFile->GetObject("CUTG",graph_cut);
+  cutFile->Close();
+
+  double mlp_cut=0.55;
+  TFile *MyFile = new TFile(output,"recreate");
+ 
+  Long64_t nentries = fChain->GetEntries();
+  Long64_t nbytes = 0, nb = 0;
+  step =(int)nentries/15;
+
+  for (Long64_t jentry=0; jentry<nentries;jentry++)
+    {
+      Long64_t ientry = LoadTree(jentry);
+      if(jentry%step==0)
+	cout<<"Progresss: "<<(double)jentry/nentries<<endl;
+      if (ientry < 0) break;
+      nb = fChain->GetEntry(jentry);   nbytes += nb;
+      // if (Cut(ientry) < 0) continue;
+
+      if(
+	 m_inv_pip_pim<500
+	 && m_inv_pip_pim>480
+	 && m_inv_p_pim<1126
+	 && m_inv_p_pim>1106
+	 && mlp_output<mlp_cut
+	 && miss_mass_kp>1077
+	 )//K0 and L(1116)
+	{
+	  dedx_lambda->Fill(pip_p,pip_dedx);
+	  missing_mass_K0_L->Fill(miss_mass_kp);
+	}
+
+            
+      //all events for final pictures
+      if(isBest_new!=1
+	 ||mlp_output<mlp_cut
+	 //||miss_mass_kp<1432 //replaced by graphical cut
+	 //||m_inv_pip_pim>410 //replaced by graphical cut
+	 ||dist_ver_to_ver<20
+	 ||(oa_lambda>20)
+	 ||!(graph_cut->IsInside(miss_mass_kp,m_inv_pip_pim))
+	 //||p_theta>20 //to clean up proton sample
+	 //||dist_pip_pim>15
+	 //||dist_pip_pim>150
+	 //||ver_p_pim_z<-5
+	 //||dist_ver_to_ver<14
+	 )
+	continue;
+
+      oryginal_spectrum->Fill(m_inv_p_pim);
+      
+      if(m_inv_p_pim<1116+sidebandmin && m_inv_p_pim>1116-sidebandmin)
+	{
+	  data->Fill(m_inv_p_pim_pip_pim);
+	  miss_m_vs_pip_pim->Fill(miss_mass_kp,m_inv_pip_pim);	  
+	}
+
+      if((m_inv_p_pim<1116.-sidebandmin && m_inv_p_pim>1116.-sidebandmax)
+	 ||(m_inv_p_pim>1116.+sidebandmin && m_inv_p_pim<1116.+sidebandmax))
+	background->Fill(m_inv_p_pim_pip_pim);
+    }
+
+  //normalize background to signal
+  TCanvas* cFit1116=new TCanvas("cFit1116");
+  cFit1116->cd();
+
+  oryginal_spectrum->Sumw2();
+   
+  TF1* fVoigt_bg= new TF1("fVoigt_bg","[0]*TMath::Voigt(x-[1],[2],[3])+pol5(4)",1090.00,1156.67);
+  TF1* fVoigt= new TF1("fVoigt","[0]*TMath::Voigt(x-[1],[2],[3])",1090.00,1156.67);
+  TF1* fbg= new TF1("fbg","pol5(0)",1090.00,1156.67);
+
+  fVoigt_bg->SetParameters(527.3,1114,2.73,1,-9266,1.7,0.0061,5.51379e-6,1.23803e-9,-5.64175e-12);
+  fVoigt_bg->SetParLimits(3,0,2);
+  fVoigt_bg->SetParLimits(1,1112,1117);
+  fVoigt_bg->SetRange(1106,1121);
+  oryginal_spectrum->Fit(fVoigt_bg,"R");
+  fVoigt_bg->SetRange(1100,1126);
+  oryginal_spectrum->Fit(fVoigt_bg,"R");
+  fVoigt_bg->SetRange(1092,1137);
+  oryginal_spectrum->Fit(fVoigt_bg,"R");
+  fVoigt_bg->SetRange(1088,1141); 
+  oryginal_spectrum->Fit(fVoigt_bg,"R");
+  fVoigt_bg->SetRange(1082,1141); 
+  oryginal_spectrum->Fit(fVoigt_bg,"R");
+
+
+  oryginal_spectrum->Draw();
+  fbg->SetParameters(fVoigt_bg->GetParameter(4),fVoigt_bg->GetParameter(5),fVoigt_bg->GetParameter(6),fVoigt_bg->GetParameter(7),fVoigt_bg->GetParameter(8),fVoigt_bg->GetParameter(9));
+  fVoigt->SetParameters(fVoigt_bg->GetParameter(0),fVoigt_bg->GetParameter(1),fVoigt_bg->GetParameter(2),fVoigt_bg->GetParameter(3));
+  fVoigt->Draw("same");
+  fVoigt->SetLineColor(kGreen);
+  fbg->Draw("same");
+  fbg->SetLineColor(kBlue);
+   
+  double intS=fVoigt->Integral(1116-sidebandmin,1116+sidebandmin)/oryginal_spectrum->GetBinWidth(1);
+  double intB=fbg->Integral(1116-sidebandmin,1116+sidebandmin)/oryginal_spectrum->GetBinWidth(1);
+  double intsideband=(fbg->Integral(1116-sidebandmax,1116-sidebandmin)+fbg->Integral(1116+sidebandmin,1116+sidebandmax))/oryginal_spectrum->GetBinWidth(1);
+  double intAll=fVoigt_bg->Integral(1116-sidebandmin,1116+sidebandmin)/oryginal_spectrum->GetBinWidth(1);
+  
+  cout<<"signal integral: "<<intS<<endl<<"beckground integral: "<<intB<<endl<<"sideband integral: "<<intsideband<<endl;
+  cout<<"all in signal range: "<<intAll<<endl;
+
+  background->Scale(intB/intsideband);
+
+  //Fill random signal
+  //TF1* L1520Spectral=new TF1("L1520Spectral","100*exp(-0.5*((x-1520)/16)**2)",xmin,xmax);
+  TF1* L1520Spectral=new TF1("L1520Spectral","TMath::BreitWigner(x,1519.5,15.6)",xmin,xmax);
+  signal->FillRandom("L1520Spectral",10000);
+
+  signal->Scale((double)nsignal/10000);
+  //Calculate residuals
+  for(int i=0;i<bin;i++)
+    {
+      resi->SetPoint(i,background->GetBinCenter(i),data->GetBinContent(i)-background->GetBinContent(i));
+      resi->SetPointError(i,background->GetBinWidth(i),TMath::Sqrt(TMath::Power(data->GetBinError(i),2)+TMath::Power(background->GetBinError(i),2)));
+    }
+
+  dedx_lambda->Write();
+  cFit1116->Write();
+  fVoigt->Write();
+  fbg->Write();
+  fVoigt_bg->Write();
+  resi->Write();
+  signal->Write();
+  background->Write();
+  data->Write();
+  oryginal_spectrum->Write();
+  missing_mass_K0_L->Write();
+  miss_m_vs_pip_pim->Write();
+  graph_cut->Write();
+  MyFile->Close();
+}
---
+++ FAT/PPIMPIPPIM/All_analysis_backup/draw_norm.C
@@ -0,0 +1,166 @@
+void normalize(TH1* hist)
+{
+  for (Int_t j=1; j<hist->GetNbinsX()+1; ++j)
+    {
+      double scale=1.0/(3.13 * TMath::Power(10,8)) *1000; /*to get micro barn*/
+      hist->SetBinContent(j, hist->GetBinContent(j) / hist->GetBinWidth(j) *scale);
+      //hist->SetBinError( j, TMath::Sqrt( hist->GetBinContent(j) ) );
+      hist->SetBinError( j, hist->GetBinError(j) / hist->GetBinWidth(j) * scale );
+      hist->GetYaxis()->SetTitle("#frac{dN}{dE} [#frac{#mu b}{MeV}]");
+    }
+
+}  
+
+int draw_norm(void)
+{
+  TFile *fileS1385 = new TFile("SB_sim_S1385pK0.root","READ");
+  TFile *fileSDpp = new TFile("SB_sim_SDppK0.root","READ");
+  TFile *fileLDpp = new TFile("SB_sim_LDppK0.root","READ");
+  TFile *fileL1520= new TFile("SB_sim_L1520pippim.root","READ");
+  TFile *fileExp= new TFile("SB_experiment.root","READ");
+  
+  //TFile *output= new TFile("pictures.root","RECREATE");
+
+  TH1F *hS1385_data = (TH1F*)fileS1385->Get("data");
+  hS1385_data->SetName("hS1385_data");
+  TH1F *hSDpp_data = (TH1F*)fileSDpp->Get("data");
+  hSDpp_data->SetName("hSDpp_data");
+  TH1F *hLDpp_data = (TH1F*)fileLDpp->Get("data");
+  hLDpp_data->SetName("hLDpp_data");
+  TH1F *hexperiment_data=(TH1F*)fileExp->Get("data");
+  hexperiment_data->SetName("hexperiment_data");
+  TH1F *hL1520_data=(TH1F*)fileL1520->Get("data");
+  hL1520_data->SetName("hL1520_data");
+  TH1F *hsum_data=(TH1F*)hS1385_data->Clone("hsum_data");
+  hsum_data->Reset();
+  
+  TH1F *hS1385_background = (TH1F*)fileS1385->Get("background");
+  hS1385_background->SetName("hS1385_background");
+  TH1F *hSDpp_background = (TH1F*)fileSDpp->Get("background");
+  hSDpp_background->SetName("hSDpp_background");
+  TH1F *hLDpp_background = (TH1F*)fileLDpp->Get("background");
+  hLDpp_background->SetName("hLDpp_background");
+  TH1F *hexperiment_background=(TH1F*)fileExp->Get("background");
+  hexperiment_background->SetName("hexperiment_background");
+  TH1F *hL1520_background=(TH1F*)fileL1520->Get("background");
+  hL1520_background->SetName("hL1520_background");
+
+  
+  TH1F *hsum_background=(TH1F*)hS1385_background->Clone("hsum_background");
+  TH1F *hclean_background=(TH1F*)hS1385_background->Clone("hclean_background");
+  TH1F *hclean_experiment=(TH1F*)hexperiment_background->Clone("hclean_experiment");
+  TH1F *hclean_L1520=(TH1F*)hL1520_background->Clone("hclean_L1520");
+  TH1F *hclean_sum=(TH1F*)hL1520_background->Clone("hclean_sum");
+  hsum_background->Reset();
+  hclean_background->Reset();
+  hclean_experiment->Reset();
+  hclean_L1520->Reset();
+  hclean_sum->Reset();
+  
+  //scale according to CS
+  double nsim=40*TMath::Power(10,6);//number of simulated events
+  double scale=3.13*TMath::Power(10,8);
+  double downscale=3;//trigger downscale for simulated events
+  double cs[4]={14.05/1000*scale/(nsim*downscale),//S1385
+		9.26/1000*scale/(nsim*downscale),//SDpp
+		29.45/1000*scale/(nsim*downscale),//LDpp
+		35.26*0.06/1000*scale/(100*100000*downscale)//L(1520)pK+->Lpi+pi-pK+
+  };
+  double cs_sig;
+  // cs in \mu barns, have to me re-calculated to mb!!
+
+  hS1385_background->Scale(cs[0]);
+  hSDpp_background->Scale(cs[1]);
+  hLDpp_background->Scale(cs[2]);
+  hL1520_background->Scale(cs[3]);
+  
+  hS1385_data->Scale(cs[0]);
+  hSDpp_data->Scale(cs[1]);
+  hLDpp_data->Scale(cs[2]);
+  hL1520_data->Scale(cs[3]);
+  
+  hsum_background->Add(hS1385_background);
+  hsum_background->Add(hSDpp_background);
+  hsum_background->Add(hLDpp_background);
+
+  hsum_data->Add(hS1385_data);
+  hsum_data->Add(hSDpp_data);
+  hsum_data->Add(hLDpp_data);
+
+  hclean_background->Add(hsum_data,hsum_background,1,-1);
+  hclean_experiment->Add(hexperiment_data,hexperiment_background,1,-1);
+  hclean_L1520->Add(hL1520_data,hL1520_background,1,-1);
+  hclean_sum->Add(hclean_L1520,hclean_background,1,1);
+  //cs_sig=1/(hclean_L1520->Integral())*20;
+  //hclean_L1520->Scale(cs_sig);    
+  
+  TCanvas *cRes=new TCanvas("cRes","cRes");
+  cRes->Divide(2,2);
+  cRes->cd(1);
+  hS1385_data->Draw();
+  hS1385_background->SetLineColor(kRed);
+  hS1385_background->Draw("same");
+  cRes->cd(2);
+  hSDpp_data->Draw();
+  hSDpp_background->SetLineColor(kRed);
+  hSDpp_background->Draw("same");
+  cRes->cd(3);
+  hLDpp_data->Draw();
+  hLDpp_background->SetLineColor(kRed);
+  hLDpp_background->Draw("same");
+  cRes->cd(4);
+  hL1520_data->Draw();
+  hL1520_background->SetLineColor(kRed);
+  hL1520_background->Draw("same");
+  
+  
+  TCanvas *cSum=new TCanvas("cSum","cSum");
+  hexperiment_data->Draw();
+  hexperiment_background->SetLineColor(kRed);
+  hexperiment_background->Draw("same");
+  hsum_data->Draw("same");
+  hsum_background->SetLineColor(kRed);
+  hsum_background->Draw("Same");
+
+  int rebin=4;
+  TCanvas *cClean=new TCanvas("cClean","cClean");
+  hclean_experiment->Draw();
+  hclean_experiment->Rebin(rebin);
+  hclean_background->SetLineColor(kRed);
+  hclean_background->Rebin(rebin);
+  hclean_background->Draw("same");
+  hclean_L1520->SetLineColor(kGreen);
+  hclean_L1520->Rebin(rebin);
+  hclean_L1520->Draw("same");
+  hclean_sum->Rebin(rebin);
+  hclean_sum->SetLineColor(kMagenta);
+  hclean_sum->Draw("same");
+  
+
+  //save all
+  TFile* output=new TFile("final_output.root","recreate");
+
+  hS1385_data->Write();
+  hSDpp_data->Write();
+  hLDpp_data->Write();
+  hexperiment_data->Write();
+  hL1520_data->Write();
+  hsum_data->Write();
+  
+  hS1385_background->Write();
+  hSDpp_background->Write();
+  hLDpp_background->Write();
+  hexperiment_background->Write();
+  hL1520_background->Write();
+  
+  hsum_background->Write();
+  hclean_background->Write();
+  hclean_experiment->Write();
+  hclean_L1520->Write();
+  hclean_sum->Write();
+  
+  cRes->Write();
+  cClean->Write();
+  cSum->Write();
+}
+
